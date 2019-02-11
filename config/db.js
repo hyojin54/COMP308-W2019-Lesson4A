b3fe020ef9de27b1e6627b9458cd3ceec4b6b9ce
Hyojin Kim <akumada54@gmail.com>
2019-02-10 21:25:22 -0500

    changed db URI

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "URI": "mongodb://localhost/test"
+    //"URI": "mongodb://localhost/test"
     //useNewUrlParser: true
-    //"URI": "mongodb://thomas:a123456@ds018258.mlab.com:18258/comp308-w2019-lesson4a"
+    "URI": "mongodb://thomas:a123456@ds018258.mlab.com:18258/comp308-w2019-lesson4a"
     
 } 
